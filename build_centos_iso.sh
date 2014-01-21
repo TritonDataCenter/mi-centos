7959fb04c4a3273ce52ad7f60b34d0ff539d5d99
Christopher Horrell <christopher@joyent.com>
2014-01-21 19:08:18 +0000

    Updated OS version to 6.5, 6.4 is deprecated

--- build_centos_iso.sh
+++ build_centos_iso.sh
@@ -3,12 +3,12 @@
 CUR_TIME=`date +%FT%TZ`
 CUSTOM_RPMS=./RPMS
 DVD_LAYOUT=/data/centos-64-iso-layout
-DVD_TITLE='Centos 6.4 Joyent'
-ISO=CentOS-6.4-x86_64-bin-DVD1.iso
+DVD_TITLE='Centos 6.5 Joyent'
+ISO=CentOS-6.5-x86_64-bin-DVD1.iso
 ISO_DIR=/data/fetched-iso
-ISO_FILENAME=./centos-64-joyent-$CUR_TIME.iso
+ISO_FILENAME=./centos-65-joyent-$CUR_TIME.iso
 KS_CFG=./ks.cfg
-MIRROR=http://mirror.stanford.edu/yum/pub/centos/6.4/isos/x86_64
+MIRROR=http://mirror.stanford.edu/yum/pub/centos/6.5/isos/x86_64
 MOUNT_POINT=/mnt
 
 function fetch_iso() {
