821586f7b2868758f87ecb98e6c177f394ddc32a
Christopher Horrell <christopher@joyent.com>
2014-11-17 14:33:01 -0500

    Update to 6.6. See IMAGE-633
    Also includes a few small improvements:
    - Removed minor version number from custom ISO file name
    - a VERSION var to make future updates easy
    - New mirror: yum.tamu.edu/centos/

--- build_centos_iso.sh
+++ build_centos_iso.sh
@@ -5,13 +5,14 @@ IFS=$'\n\t'
 
 CUR_TIME=`date +%FT%TZ`
 CUSTOM_RPMS=./RPMS
-DVD_LAYOUT=/data/centos-65-iso-layout
-DVD_TITLE='Centos 6.5 Joyent'
-ISO=CentOS-6.5-x86_64-bin-DVD1.iso
+DVD_LAYOUT=/data/centos-6-iso-layout
+VERSION="6.6"
+DVD_TITLE="CentOS ${VERSION} Joyent"
+ISO=CentOS-${VERSION}-x86_64-bin-DVD1.iso
 ISO_DIR=/data/fetched-iso
-ISO_FILENAME=./centos-65-joyent.iso
+ISO_FILENAME=./centos-6-joyent.iso
 KS_CFG=./ks.cfg
-MIRROR=http://mirror.cs.vt.edu/pub/CentOS/6.5/isos/x86_64
+MIRROR=http://yum.tamu.edu/centos/${VERSION}/isos/x86_64
 MOUNT_POINT=/mnt/centos6
 
 function fetch_iso() {
